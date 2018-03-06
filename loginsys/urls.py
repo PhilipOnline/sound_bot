561b8345c94fdd73286b937f10220ed884875b50
PhilipOnline <philip.bormotov@gmail.com>
2018-03-06 11:42:51 +0300

    - добавлен метод авторизации и выхода - отредактированы шаблоны для отображения кнопок Войти - Выйти

--- loginsys/urls.py
+++ loginsys/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url,include
 from . import views
 
 urlpatterns = [
-    url(r'^login/', views.login, name='login'),
-    url(r'^logout/', views.logout, name='logout'),
+    url(r'^login', views.login, name='login'),
+    url(r'^logout', views.logout, name='logout'),
 
 ]
--- loginsys/views.py
+++ loginsys/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404  # render page, return data or 404 page
+from django.http import HttpResponseRedirect
+from django.contrib import auth
 
-# Create your views here.
+def login(request):
+    context = {}
+    if request.method == 'POST':
+        username = request.POST.get('username',)
+        password = request.POST.get('password', )
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return HttpResponseRedirect('/')
+        else:
+            login_error = 'User not exist'
+            context = {'login_error': login_error}
+            return render(request, 'loginsys/login.html', context)
+    else:
+        return render(request, 'loginsys/login.html', context)
+
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect('/')

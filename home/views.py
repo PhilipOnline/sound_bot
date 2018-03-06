7d56204af8875182c338fafb083b1e6ecd68ab10
PhilipOnline <philip.bormotov@gmail.com>
2018-03-06 18:24:03 +0300

    - создан класс Cart (корзина) с методами добавить, удалить, сохранить - добавлена форма добавления в корзину в вьюхе карточки товара - во вьюхе корзины добавлены методы добавления, удаления товара и рендеринга шаблона - создан шаблон корзины и отредактирован шаблон карточки товара

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
+from cart.forms import CartAddProductForm
 
 def home(request):
     return render(request, 'home/home.html')
@@ -21,4 +22,6 @@ def ProductList(request, category_slug=None):
 # Страница товара
 def ProductDetail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug)
-    return render(request, 'home/product_detail.html', {'product': product})
+    cart_product_form = CartAddProductForm()
+    return render(request, 'home/product_detail.html', {'product': product,
+                                                        'cart_product_form': cart_product_form})
--- site_s/urls.py
+++ site_s/urls.py
@@ -21,8 +21,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-	url(r'', include('home.urls')),
     url(r'auth/', include('loginsys.urls')),
+    url(r'^cart/', include('cart.urls')),
+    url(r'', include('home.urls')),
 
 ]
 
